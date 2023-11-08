a8464c9cbda950fda70af3b4dc9ad4ce27e28dad
DaniruBu <buda-0000@mail.ru>
2023-11-08 06:56:32 +0300

    add Lab2: 1lvl, 2lvl, 3lvl; Lab3: 1lvl, 2lvl

---
+++ LAB2/Program.cs
@@ -0,0 +1,141 @@
+﻿void Level1_2()
+{
+    Console.WriteLine("Level1_2");
+    double x;
+    double y;
+    bool l = false;
+    Console.Write("Enter x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+    if (y >= 0 && y + Math.Abs(x) <= 1)
+        l = true;
+    else l = false;
+    Console.WriteLine(l);
+}
+
+void Level1_10()
+{
+    Console.WriteLine("Level1_10");
+    double x, y;
+    Console.Write("Enter x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+    if (x <= -1)
+        y = 1;
+    else if (x > -1 && x <= 1)
+        y = -x;
+    else y = -1;
+    Console.WriteLine(y);
+}
+void Level2_1()
+{
+    Console.WriteLine("Level2_1");
+    double n, s = 0;
+    Console.Write("Enter num: ");
+    n = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter height: ");
+    for (int i = 1; i <= n; i++)
+        s += Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Average height = {s / n}");
+
+}
+void Level2_9()
+{
+    Console.WriteLine("Level2_9");
+    double r, n, b = 0;
+    Console.Write("Enter num: ");
+    n = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter results: ");
+    for (int i = 1; i <= n; i++)
+    {
+        r = Convert.ToDouble(Console.ReadLine());
+        if (r > b) b = r;
+    }
+    Console.WriteLine($"Best result = {b}");
+}
+void Level3_4()
+{
+    Console.WriteLine("Level3_4");
+    double x, y, r1, r2, n = 0;
+    Console.Write("Enter inner radius: ");
+    r1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter outer radius: ");
+    r2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter coor");
+    Console.Write("Enter x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+    while (true)
+    {
+        if ((y * y + x * x > r1 * r1) && (y * y + x * x < r2 * r2))
+            n += 1;
+        Console.Write("Enter x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        if (x == 1000) break;
+        Console.Write("Enter y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"num dots = {n}");
+    }
+    Console.Write($"num dots = {n}");
+}
+void Level3_11()
+{
+    Console.WriteLine("Level3_11");
+    double n = 0;
+    n = Convert.ToInt64(Console.ReadLine());
+    double n1 = 0, a, b, c, d;
+    int res = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+        c = Convert.ToDouble(Console.ReadLine());
+        d = Convert.ToDouble(Console.ReadLine());
+        if (a == 2 || b == 2 || c == 2 || d == 2)
+            res += 1;
+        n1 = n1 + ((a + b + c + d) / 4) / 2;
+    }
+    Console.WriteLine($"{res} {n1}");
+}
+void Level3_12()
+{
+    Console.WriteLine("Level3_12");
+    while (true)
+    {
+        long r;
+        r = Convert.ToInt64(Console.ReadLine());
+        if (r == -1)
+            break;
+        Console.WriteLine($"{r * r} {r * r * 3,14} {(Math.Sqrt(3) * r * r) / 4}");
+    }
+}
+void Level3_13()
+{
+    Console.WriteLine("Level3_13");
+    double s = 0;
+
+    while (true)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a == -1)
+            break;
+        string type = Convert.ToString(Console.ReadLine());
+        if (type == "ПР")
+            s = a * b;
+        if (type == "КЦ")
+            s = Math.Abs(Math.PI * a * a - Math.PI * b * b);
+        if (type == "РБТ")
+            s = a * Math.Sqrt(4 * b * b - a * a) / 4;
+
+        Console.WriteLine($"площадь {type} равняется {s}");
+    }
+}
+// Level1_2();
+// Level1_10();
+// Level2_1();
+// Level2_9();
+
+Level3_13();
+
---
+++ LAB3/Program.cs
@@ -0,0 +1,397 @@
+﻿using System;
+// LVL 1
+
+void lvl1_6()
+{
+    int[] x = new int[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        x[i] = Convert.ToInt32(Console.ReadLine());
+        sum += x[i] * x[i];
+    }
+    Console.WriteLine($"L = {Math.Pow(sum, 0.5)}");
+}
+
+
+void lvl1_10()
+{
+    Console.Write("P: ");
+    int P = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Q: ");
+    int Q = Convert.ToInt32(Console.ReadLine());
+    int[] x = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        x[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if ((x[i] > P) && (x[i] < Q))
+            count++;
+        Console.Write($"{x[i]} ");
+    }
+    Console.Write($"count = {count} ");
+}
+
+
+void lvl1_11()
+{
+    int[] x = new int[10];
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        x[i] = Convert.ToInt32(Console.ReadLine());
+        if (x[i] > 0)
+            count++;
+    }
+    int[] y = new int[count];
+    int j = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (x[i] > 0)
+        {
+            y[j] = x[i];
+            Console.Write($"{y[j]} ");
+            j++;
+        }
+    }
+}
+
+
+void lvl1_12()
+{
+    int[] x = new int[8];
+    int output = 0;
+    int j = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        x[i] = Convert.ToInt32(Console.ReadLine());
+        if (x[i] < 0)
+        {
+            output = x[i];
+            j = i;
+        }
+
+    }
+    Console.WriteLine($"{output}, {j}");
+}
+
+void lvl1_13()
+{
+    int[] x = new int[10];
+    int count_ch = 0;
+    int count_not = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        x[i] = Convert.ToInt32(Console.ReadLine());
+        if (x[i] % 2 == 0)
+            count_ch++;
+        else count_not++;
+    }
+
+    int[] x_ch = new int[count_ch];
+    int j_ch = 0;
+    int[] x_not = new int[count_not];
+    int j_not = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (x[i] % 2 == 0)
+        {
+            x_ch[j_ch] = x[i];
+            j_ch++;
+        }
+        else
+        {
+            x_not[j_not] = x[i];
+            j_not++;
+        }
+
+    }
+    Console.WriteLine($"Ch: ");
+    for (int i = 0; i < count_ch; i++)
+    {
+        Console.Write($"{x_ch[i]} ");
+    }
+    Console.WriteLine($"\nNot: ");
+    for (int i = 0; i < count_not; i++)
+    {
+        Console.Write($"{x_not[i]} ");
+    }
+}
+
+
+//Console.WriteLine("lvl1_6");
+//lvl1_6();
+//Console.WriteLine("lvl1_10");
+//lvl1_10();
+//Console.WriteLine("lvl1_11");
+//lvl1_11();
+//Console.WriteLine("lvl1_12");
+//lvl1_12();
+//Console.WriteLine("lvl1_13");
+//lvl1_13();
+
+//LVL 2
+
+void lvl2_5()
+{
+    int count = 7;
+    int[] input = { -9, 8, 23, 0, -7, -5, 1, 98 };
+    int ct = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{input[i]} ");
+        if (input[i] < 0)
+            ct++;
+    }
+    Console.Write("\n");
+    int[] output = new int[ct];
+    ct = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (input[i] < 0)
+        {
+            output[ct] = input[i];
+            Console.Write($"{output[ct]} ");
+            ct++;
+        }
+    }
+}
+
+void lvl2_6()
+{
+    int ct = 5;
+    int[] arr = new int[ct];
+    int sum = 0;
+    Console.Write("Enter P: ");
+    int p = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < ct; i++)
+    {
+        Console.Write($"Enter x{i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        sum += arr[i];
+    }
+    int sr = sum / ct;
+    int output = 0;
+    double min = 9999999;
+    for (int i = 0; i < ct; i++)
+    {
+        int pr = Math.Abs(arr[i] - sr);
+        if (min > pr)
+        {
+            min = pr;
+            output = i;
+        }
+    }
+    int[] arrout = new int[ct + 1];
+    int j = 0;
+    for (int i = 0; i < ct; i++)
+    {
+        if (i == output)
+        {
+            arrout[j] = arr[i];
+            j++;
+            arrout[j] = p;
+            j++;
+        }
+        else
+        {
+            arrout[j] = arr[i];
+            j++;
+        }
+    }
+    for (int i = 0; i < ct + 1; i++)
+        Console.Write($"{arrout[i]} ");
+}
+
+//1 9 3 4 5 6 7 0
+//среднее от 3 4 5 6 7
+void lvl2_9()
+{
+    int count = 8;
+    int[] arr = new int[count];
+    int min = 99999999;
+    int min_index = 0;
+    int max = 0;
+    int max_index = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            min_index = i;
+        }
+        else if (arr[i] > max)
+        {
+            max = arr[i];
+            max_index = i;
+        }
+    }
+    int k = 0;
+    int l = 0;
+    if (min_index > max_index)
+    {
+        k = min_index;
+        l = max_index;
+    }
+    else
+    {
+        k = max_index;
+        l = min_index;
+    }
+    int sum = 0;
+    for (int i = l + 1; i < k; i++)
+    {
+        sum += arr[i];
+    }
+    Console.Write($"{sum / (k - l - 1)} ");
+}
+
+
+void lvl2_10()
+{
+    int count = 10;
+    int[] arr = new int[count];
+    int min = 99999999;
+    int ct_min = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if ((arr[i] > 0) && (arr[i] < min))
+        {
+            min = arr[i];
+            ct_min = 0;
+        }
+    }
+
+    int[] arr_out = new int[count - ct_min];
+    int j = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i] != min)
+        {
+            arr_out[j] = arr[i];
+            Console.Write($"{arr_out[j]} ");
+            j++;
+        }
+    }
+
+}
+
+void lvl2_11()
+{
+    int count = 10;
+    int[] arr = new int[count];
+    int min = 99999999;
+    int min_index = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            min_index = i;
+        }
+    }
+    int[] arrout = new int[count - 1];
+    int j = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (i != min_index)
+        {
+            arrout[j] = arr[i];
+            Console.Write($"{arrout[j]} ");
+            j++;
+        }
+    }
+}
+
+void lvl2_13()
+{
+    int count = 10;
+    int[] arr = new int[count];
+    int max = 0;
+    int max_index = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if ((arr[i] > max) && (i % 2 == 0))
+        {
+            max = arr[i];
+            max_index = i;
+        }
+    }
+    for (int i = 0; i < count; i++)
+    {
+        if (i != max_index)
+            Console.Write($"{arr[i]} ");
+        else Console.Write($"{i} ");
+    }
+}
+
+void lvl2_15()
+{
+    int k = 4;
+    int n = 6;
+    int[] A = new int[n];
+    int m = 3;
+    int[] B = new int[m];
+    Console.WriteLine("A");
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        A[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("B");
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"x{i + 1}: ");
+        B[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (i == k - 1)
+        {
+            Console.Write($"{A[i]} ");
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"{B[j]} ");
+            }
+        }
+        Console.Write($"{A[i]} ");
+    }
+
+}
+
+//Console.WriteLine("LVL2_5");
+//lvl2_5();
+//Console.WriteLine("LVL2_6");
+//lvl2_6();
+Console.WriteLine("LVL2_9");
+lvl2_9();
+//Console.WriteLine("LVL2_10");
+//lvl2_10();
+//Console.WriteLine("LVL2_11");
+//lvl2_11();
+//Console.WriteLine("LVL2_13");
+//lvl2_13();
+//Console.WriteLine("LVL2_15");
+//lvl2_15();
+
+
+
+
